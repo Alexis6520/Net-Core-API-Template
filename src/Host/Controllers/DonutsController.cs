143c276881df258e98979d6b9f13ffba825526fc
Alexis6520 <alexis_nova@outlook.com>
2025-11-23 18:36:03 -0500

    Se agrega xml de documentación

--- src/Host/Controllers/DonutsController.cs
+++ src/Host/Controllers/DonutsController.cs
@@ -7,6 +7,11 @@ namespace Host.Controllers
 {
     public class DonutsController(IMediator mediator) : CustomController(mediator)
     {
+        /// <summary>
+        /// Crea una dona
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns>Id de la dona creada</returns>
         [HttpPost]
         [ProducesResponseType<Response<int>>(StatusCodes.Status201Created)]
         public async Task<IActionResult> Create(CreateDonutCommand command)
